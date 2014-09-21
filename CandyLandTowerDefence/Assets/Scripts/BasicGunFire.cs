7b60cea5f685f658f685a5457bc237fa466f72eb
blochowcizr <blochowiczr@yahoo.com>
2014-09-21 16:07:15 -0500

    CandyLandDefence V0
    This is my base Unity Project

---
+++ CandyLandTowerDefence/Assets/Scripts/BasicGunFire.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicGunFire : MonoBehaviour {
+
+    public Rigidbody datBullet;
+    public int bulletSpeed;
+    public double shootTimer = 0;
+    public double time2Shoot = 2;
+    //public bool hasTarget = false; 
+
+	// Use this for initialization
+	void Start () {
+	    //FollowEnemyTest followEnemyTest = this.GetComponent<FollowEnemyTest>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FollowEnemyTest followEnemyTest = this.GetComponent<FollowEnemyTest>();
+        shootTimer += Time.deltaTime;
+        if(followEnemyTest.hasTarget)
+        {
+            if(shootTimer >= time2Shoot)
+            {
+                shootTimer = 0;
+                Debug.Log("Boom!!!");
+                FireGun();
+            }
+        }
+	}
+
+    public void FireGun()
+    {
+        Rigidbody bulletClone = Instantiate(datBullet, transform.position, transform.rotation) as Rigidbody;
+        bulletClone.rigidbody.useGravity = false; 
+        bulletClone.velocity = transform.TransformDirection(Vector3.forward*bulletSpeed);
+        Destroy(bulletClone.gameObject, 2);
+    }
+
+    /*public void setHasTarget(bool setInput)
+    {
+        this.GetComponent<BasicGunFire>().hasTarget = setInput;
+    }*/
+}
---
+++ CandyLandTowerDefence/Assets/Scripts/FollowEnemyTest.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowEnemyTest : MonoBehaviour {
+
+    //public Object datEnemy;
+    public GameObject datEnemy;
+    public int maxDist = 20;
+    public bool hasTarget = false;
+    //BasicGunFire BasicGunFire;
+
+    // Use this for initialization
+	void Start () {
+        
+	}
+
+    public GameObject GetClosestEnemy()
+    {
+        GameObject[] demEnemies;
+        demEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject closestEnemy = null;
+        float distance = Mathf.Infinity;
+        Vector3 disPosition = transform.position;
+        foreach (GameObject Enemy in demEnemies)
+        {
+            Vector3 diff = Enemy.transform.position - disPosition;
+            float curDustance = diff.sqrMagnitude;
+            if (curDustance < distance)
+            {
+                closestEnemy = Enemy;
+                distance = curDustance;
+            }
+        }
+        return closestEnemy;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject thisGun = GameObject.Find("BasicTestGun");
+        BasicGunFire basicGunFire = thisGun.GetComponent<BasicGunFire>();
+        datEnemy = GetClosestEnemy();
+        //Debug.Log(FindClosestEnemy().name);
+        //datEnemy = FindClosestEnemy();
+        Debug.Log(GetClosestEnemy().name);
+        float enemyDist = Vector3.Distance(this.transform.position, datEnemy.transform.position);
+        Debug.Log(enemyDist);
+        //BasicGunFire = this.GetComponent<BasicGunFire>();
+        if(enemyDist <= maxDist)
+        {
+            //this.gameObject.GetComponent<BasicGunFire>().setHasTarget(true);
+            //basicGunFire.hasTarget = true; 
+            //this.GetComponent<BasicGunFire>().hasTarget = true; 
+            //basicGunFire.setHasTarget(true);
+            hasTarget = true;
+            this.transform.LookAt(datEnemy.transform);
+        }
+        if(enemyDist >= maxDist)
+        {
+            //BasicGunFire.setHasTarget(false);
+            //this.GetComponent<BasicGunFire>().hasTarget = false; 
+            //basicGunFire.hasTarget = false; 
+            //basicGunFire.setHasTarget(false);
+            hasTarget = false; 
+        }
+	}
+
+
+    
+}
