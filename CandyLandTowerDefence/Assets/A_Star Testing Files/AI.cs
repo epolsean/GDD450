1edefd76f3061bd36814fc0422d73ebcecb85833
polseane9067 <polseane9067@my.uwstout.edu>
2014-09-23 18:59:11 -0500

    A_Star Pathfinding Test

---
+++ CandyLandTowerDefence/Assets/A_Star Testing Files/AI.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pathfinding;
+
+public class AI : MonoBehaviour {
+
+    public Vector3 targetPosition;
+    public Seeker seeker;
+    public CharacterController controller;
+    public Path path;
+    public float speed = 100;
+    public float nextWaypointDistance = 3.0f;
+    int currentWaypoint = 0;
+
+    void Start()
+    {
+        targetPosition = GameObject.FindWithTag("GroundTargetObject").transform.position;
+        GetNewPath();
+    }
+
+    public void GetNewPath()
+    {
+        seeker.StartPath(transform.position, targetPosition, OnPathComplete);
+    }
+
+    void OnPathComplete(Path newPath)
+    {
+        if (!newPath.error)
+        {
+            path = newPath;
+            currentWaypoint = 0;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (path == null)
+        {
+            return;
+        }
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            return;
+        }
+
+        Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized;
+
+        dir *= speed * Time.fixedDeltaTime;
+
+        controller.SimpleMove(dir);
+
+        if (Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]) < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+    }
+}
