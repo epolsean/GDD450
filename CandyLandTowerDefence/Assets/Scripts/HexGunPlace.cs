99c35a167e7e5a30d5822f61071beabdd4b6f735
blochowcizr <blochowiczr@yahoo.com>
2014-09-24 16:29:18 -0500

    TestPlacingGuns
    added a new scene testingGunPlacing where if you click on a plate it
    will spawn a Basic gun there

---
+++ CandyLandTowerDefence/Assets/Scripts/HexGunPlace.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexGunPlace : MonoBehaviour {
+
+    public GameObject BasicGun;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseOver()
+    {
+        renderer.material.color = Color.blue;
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(BasicGun, this.transform.position, this.transform.rotation);
+        }
+    }
+
+    void OnMouseExit()
+    {
+        renderer.material.color = Color.white;
+    }
+}
