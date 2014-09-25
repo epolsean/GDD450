78354ce955c585060b6c3bb14f1e8ce43158dd43
blochowcizr <blochowiczr@yahoo.com>
2014-09-25 17:04:06 -0500

    Updated gun & Tower Placement

--- CandyLandTowerDefence/Assets/Scripts/HexGunPlace.cs
+++ CandyLandTowerDefence/Assets/Scripts/HexGunPlace.cs
@@ -4,10 +4,11 @@ using System.Collections;
 public class HexGunPlace : MonoBehaviour {
 
     public GameObject BasicGun;
+    public GameObject BasicHexWall;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
@@ -17,12 +18,27 @@ public class HexGunPlace : MonoBehaviour {
 
     void OnMouseOver()
     {
-        if (tag == "SlotOpen" || tag == "SlotWall")
+        if (tag == "SlotOpen")
+        {
+            renderer.material.color = Color.green;
+            if (Input.GetMouseButtonDown(0))
+            {
+                Instantiate(BasicGun, this.transform.position+Vector3.up, this.transform.rotation);
+                Instantiate(BasicHexWall, this.transform.position, this.transform.rotation);
+                tag = "SlotClosed";
+            }
+            if(Input.GetMouseButtonDown(1))
+            {
+                Instantiate(BasicHexWall, this.transform.position, this.transform.rotation);
+                tag = "SlotWall";
+            }
+        }
+        else if(tag == "SlotWall")
         {
             renderer.material.color = Color.blue;
             if (Input.GetMouseButtonDown(0))
             {
-                Instantiate(BasicGun, this.transform.position, this.transform.rotation);
+                Instantiate(BasicGun, this.transform.position + Vector3.up, this.transform.rotation);
                 tag = "SlotClosed";
             }
         }
