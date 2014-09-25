bb9d669cb5c363b7aa59ac66f034897173b1533b
polseane9067 <polseane9067@my.uwstout.edu>
2014-09-25 10:34:46 -0500

    WaveController V2

--- CandyLandTowerDefence/Assets/Scripts/WaveSetup.cs
+++ CandyLandTowerDefence/Assets/Scripts/WaveSetup.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class WaveSetup : MonoBehaviour {
 
-    public int wave = 0;
+    public Button start;
+    public int wave = 1;
     public Transform[] spawnPoints;
     public GameObject[] enemyTypes;
     public Vector2 spawnTime = new Vector2(2, 8);
@@ -17,14 +19,15 @@ public class WaveSetup : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        start.onClick.AddListener(() => { buildMode = false; });
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (buildMode == false)
         {
-            if (spawns <= totalEnemies)
+            start.enabled = false;
+            if (spawns < totalEnemies)
             {
                 if (timeSet == false)
                 {
@@ -35,15 +38,15 @@ public class WaveSetup : MonoBehaviour {
                 else if (timeSet == true && Time.time >= spawnRate)
                 {
 
-                    if (spawnNumber >= 40 && spawnNumber <= 100)
+                    if (spawnNumber >= 30 && spawnNumber <= 100)
                     {
                         selectedEnemy = 0;
                     }
-                    else if (spawnNumber >= 10 && spawnNumber < 40)
+                    else if (spawnNumber >= 5 && spawnNumber < 30)
                     {
                         selectedEnemy = 1;
                     }
-                    else if (spawnNumber >= 0 && spawnNumber < 10)
+                    else if (spawnNumber >= 0 && spawnNumber < 5)
                     {
                         selectedEnemy = 2;
                     }
@@ -52,6 +55,14 @@ public class WaveSetup : MonoBehaviour {
                     spawns++;
                     timeSet = false;
                 }
+            }
+            else
+            {
+                spawns = 0;
+                wave++;
+                totalEnemies = 4 * wave;
+                buildMode = true;
+                start.enabled = true;
             }
         }
 	}
