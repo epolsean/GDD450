6f4e03d0d9894ba06eba1740d0d5940f6a2b80f6
blochowcizr <blochowiczr@yahoo.com>
2014-09-26 11:48:48 -0500

    RandomGunBallColors
    i added script to the bullets or now called GunBalls to randomly
    generate with a color

---
+++ CandyLandTowerDefence/Assets/Scripts/RandGunBall.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandGunBall : MonoBehaviour {
+
+    public int RanGunBallColor = 0;
+
+	// Use this for initialization
+	void Start () {
+        RanGunBallColor = Random.Range(0, 3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(RanGunBallColor>=3)
+        {
+            this.renderer.material.color = Color.red;
+        }
+        else if (RanGunBallColor >= 2)
+        {
+            this.renderer.material.color = Color.green;
+        }
+        else if (RanGunBallColor >= 1)
+        {
+            this.renderer.material.color = Color.yellow;
+        }
+        else
+        {
+            this.renderer.material.color = Color.blue;
+        }
+	}
+}
