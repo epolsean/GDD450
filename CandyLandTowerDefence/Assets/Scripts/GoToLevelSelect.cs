eadc8aec5ba4f0bfb44e0a914fbe7982c5550f2a
schimmelc <schimmelc4422@my.uwstout.edu>
2014-09-30 14:14:25 -0500

    Update Scenes and UI
    Can go from start screen to settings/credits/level select and from those
    back

---
+++ CandyLandTowerDefence/Assets/Scripts/GoToLevelSelect.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToLevelSelect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Application.LoadLevel("LevelSelectScene");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
