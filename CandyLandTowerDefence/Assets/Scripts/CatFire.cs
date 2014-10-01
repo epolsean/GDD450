a464df59d8125b6a6687595cf237c8c357a05a36
blochowcizr <blochowiczr@yahoo.com>
2014-10-01 16:28:02 -0500

    StartedCatapult
    I now have a semi-working Catapult but it's very semi.

---
+++ CandyLandTowerDefence/Assets/Scripts/CatFire.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatFire : MonoBehaviour {
+
+    public GameObject CatBase;
+    public GameObject CatPivot;
+    public Rigidbody datProj;
+    public int projSpeed = 9;
+    public double shootTimer = 0;
+    public double time2Shoot = 2;
+    //public bool hasTarget = false; 
+
+	// Use this for initialization
+	void Start () {
+	    //FollowEnemyTest followEnemyTest = this.GetComponent<FollowEnemyTest>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FollowEnemyTestCatapult followEnemyTestCatapult = CatBase.GetComponent<FollowEnemyTestCatapult>();
+        shootTimer += Time.deltaTime;
+        if(followEnemyTestCatapult.hasTarget)
+        {
+            if(shootTimer >= time2Shoot)
+            {
+                shootTimer = 0;
+                //Transform originalTrans = CatPivot.transform;
+                //CatPivot.transform.rotation = Quaternion.Euler(150, 0, 0);
+                Debug.Log("Boom!!!");
+                FireGun();
+                //CatPivot.transform.rotation = originalTrans.rotation;
+                //CatPivot.transform.rotation = Quaternion.Euler(75, 0, 0);
+            }
+        }
+	}
+
+    public void FireGun()
+    {
+        Rigidbody bulletClone = Instantiate(datProj, transform.position, transform.rotation) as Rigidbody;
+        //bulletClone.rigidbody.useGravity = false; 
+        bulletClone.velocity = transform.TransformDirection(Vector3.forward*projSpeed);
+        Destroy(bulletClone.gameObject, 2);
+    }
+
+    /*public void setHasTarget(bool setInput)
+    {
+        this.GetComponent<BasicGunFire>().hasTarget = setInput;
+    }*/
+}
---
+++ CandyLandTowerDefence/Assets/Scripts/FollowEnemyTestCatapult.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowEnemyTestCatapult : MonoBehaviour {
+
+    //public Object datEnemy;
+    public GameObject datEnemy;
+    public GameObject EndOfBarrel;
+    public int maxDist = 10;
+    public bool hasTarget;
+    public bool isFPS;
+
+    public int damp = 1;
+    //BasicGunFire BasicGunFire;
+
+    // Use this for initialization
+	void Start () {
+        hasTarget = false;
+        isFPS = false; 
+	}
+
+    public GameObject GetClosestEnemy()
+    {
+        GameObject[] demEnemies;
+        demEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject closestEnemy = null;
+        float distance = Mathf.Infinity;
+        Vector3 disPosition = transform.position;
+        foreach (GameObject Enemy in demEnemies)
+        {
+            Vector3 diff = Enemy.transform.position - disPosition;
+            float curDustance = diff.sqrMagnitude;
+            if (curDustance < distance)
+            {
+                closestEnemy = Enemy;
+                distance = curDustance;
+            }
+        }
+        return closestEnemy;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //GameObject thisGun = GameObject.Find("BasicTestGun");
+        //BasicGunFire basicGunFire = EndOfBarrel.GetComponent<BasicGunFire>();
+        datEnemy = GetClosestEnemy();
+        float enemyDist = Vector3.Distance(this.transform.position, datEnemy.transform.position);
+        Debug.Log(enemyDist);
+
+        if(isFPS == false)
+        {
+            if(enemyDist <= maxDist)
+            {
+                hasTarget = true;
+                var rotationAngle = Quaternion.LookRotation(datEnemy.transform.position - transform.position); 
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotationAngle, Time.deltaTime * damp);
+                transform.rotation = new Quaternion(0, this.transform.rotation.y, 0, this.transform.rotation.w); 
+            }
+            if(enemyDist >= maxDist)
+            {
+                hasTarget = false; 
+            }
+        }
+	}
+
+
+    
+}
