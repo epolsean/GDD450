6226d5183db03449619d553dcd93c1cc1303f5e2
blochowcizr <blochowiczr@yahoo.com>
2014-10-06 16:29:28 -0500

    NewUnityProject
    created a new unity project and made some placeholders for the pawns and
    tiles

---
+++ ArchonClone/Assets/Scripts/TileProperties.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileProperties : MonoBehaviour {
+
+    public GameObject UnitOnTile = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseOver()
+    {
+        renderer.material.color = Color.green;
+    }
+
+    void OnMouseExit()
+    {
+        if(this.tag == "WhiteTile")
+        {
+            renderer.material.color = Color.white;
+        }
+        if (this.tag == "BlackTile")
+        {
+            renderer.material.color = Color.black;
+        }
+    }
+}
