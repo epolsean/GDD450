b41aad04577880f6d6571d85aeece29325f08092
blochowcizr <blochowiczr@yahoo.com>
2014-10-07 17:03:49 -0500

    BunchOfStuff
    i added object to spawn in the two basic units for each side on
    specified tiles
    i created a system to pick one unit by clicking on the tile it is
    located on with the left mouse button and using the wasd keys you can
    move it around.
    if you wish to move a different unity all you need to do is click on the
    tile it is located on, this works for both white units and black units.
    i think that's all i did so far but i'm not sure.

---
+++ ArchonClone/Assets/Scripts/WhitePawnProperties.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhitePawnProperties : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider Tile)
+    {
+        if(Tile.tag == "WhiteTile" || Tile.tag == "BlackTile")
+        {
+            Tile.GetComponent<TileProperties>().UnitOnTile = this.gameObject;
+        }
+    }
+    void OnTriggerExit(Collider Tile)
+    {
+        Tile.GetComponent<TileProperties>().UnitOnTile = null;
+    }
+}
