9ab3f6dce94ae79b71ac2dd4d0baa5652f583d94
schimmelc <schimmelc4422@my.uwstout.edu>
2014-10-17 00:22:32 -0500

    Menu Music

---
+++ ArchonClone/Assets/MenuMusicController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuMusicController : MonoBehaviour {
+
+    private static MenuMusicController instance = null;
+    public static MenuMusicController Instance
+    {
+        get { return instance; }
+    }
+    void Awake() 
+    {
+        if (instance != null && instance != this) 
+        {
+            Destroy(this.gameObject);
+            return;
+        } 
+        else 
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+    void Update()
+    {
+        if (Application.loadedLevelName == "TestingHexTiles")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
