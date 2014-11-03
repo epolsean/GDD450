3ad6fcaa89e6fdb9317cf5dd2c14d50bc49e685a
schimmelc <schimmelc4422@my.uwstout.edu>
2014-11-03 13:03:41 -0600

    Added different map battle scenes
    Also updated sensitivity to work and be changeable in the settings scene

--- ArchonClone/Assets/Scripts/ChangeSensitivity.cs
+++ ArchonClone/Assets/Scripts/ChangeSensitivity.cs
@@ -7,8 +7,24 @@ public class ChangeSensitivity : MonoBehaviour {
     public bool player2 = true;
     public bool x_Sens = true;
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+    {
+        if (player2 && x_Sens)
+        {
+            GetComponent<Slider>().value = Player2MovementController.xSensitivity;
+        }
+        else if (player2 && !x_Sens)
+        {
+            GetComponent<Slider>().value = Player2MovementController.ySensitivity;
+        }
+        else if (!player2 && x_Sens)
+        {
+            GetComponent<Slider>().value = Player1MovementController.xSensitivity;
+        }
+        else if (!player2 && !x_Sens)
+        {
+            GetComponent<Slider>().value = Player1MovementController.ySensitivity;
+        }
 	}
 	
 	// Update is called once per frame
