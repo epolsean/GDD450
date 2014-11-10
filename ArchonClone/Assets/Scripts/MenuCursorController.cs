029cf75a334952ead865c1a942a1b4ee3652a1c5
schimmelc <schimmelc4422@my.uwstout.edu>
2014-11-10 13:22:35 -0600

    Cursor for menus and back button for select mode
    If using a controller the cursor will disappear and the user can select
    buttons by moving from one to the other with the joystick and selecting
    with A(button).
    Also put in a back button from the select mode scene to return to main
    menu

---
+++ ArchonClone/Assets/Scripts/MenuCursorController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class MenuCursorController : MonoBehaviour 
+{
+    GameObject eSystem;
+    public GameObject firstSelected;
+    Vector3 moveDirection = Vector3.zero;
+    float speed = 5.0F;
+
+    public GameObject cursor;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (Input.GetJoystickNames().Length != 0)
+        {
+            eSystem = GameObject.Find("EventSystem");
+            eSystem.GetComponent<EventSystem>().SetSelectedGameObject(firstSelected, new BaseEventData(eSystem.GetComponent<EventSystem>()));
+            gameObject.SetActive(false);
+        }
+        Screen.showCursor = false;
+
+        cursor.transform.position = new Vector3(50, 50, 0);
+    }
+
+
+    void Update()
+    {
+        cursor.transform.position = new Vector3(Input.mousePosition.x + 6, Input.mousePosition.y - 6, Input.mousePosition.z);
+    }
+}
