0aff9937b88cdb74e55198af6085b475537ff865
blochowcizr <blochowiczr@yahoo.com>
2014-11-10 14:55:57 -0600

    FixedTutorialGridView Bug
    i've added the other 2 pieces for the tutorial and i started
    implementing the enemy AI Script

---
+++ ArchonClone/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public GameObject SelectRandP(string TagName)
+    {
+        GameObject selectedPiece = null;
+        //collect all pieces with given tag...
+        
+        //randomly select one of these pieces...
+
+        return selectedPiece;
+    }
+
+    public GameObject FindNearestPlayerP(GameObject AIPiece, string PlayerTag)
+    {
+        GameObject PlayerPiece = null;
+        //collect all pieces with given tag...
+
+        //find the player piece that is closest to selected AIPiece...
+
+        return PlayerPiece; 
+    }
+
+    public void SetTargetAI()//only called if PlayerPiece within radius
+    {
+        //this sets the target for the AI Opponent and moves initiates combat...
+    }
+}
