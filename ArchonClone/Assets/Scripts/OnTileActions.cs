2a4a44099fdaf1cd3f341c2e76cb4ff3fd70787a
blochowcizr <blochowiczr@yahoo.com>
2015-01-28 10:17:50 -0600

    Github told me to
    I have no idea why all these files needed to commit all i added was two
    scripts

---
+++ ArchonClone/Assets/Scripts/OnTileActions.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnTileActions : MonoBehaviour {
+
+    public GameObject PieceOnTile = null;
+    public GameObject AtkPiece = null; 
+    public GameObject TileNode;
+    public Texture AlienTexture;
+    public Texture SynthTexture;
+    public Texture NuturalTexture = null; 
+    public int TilePowerBooster = 0;
+    public enum TileType { Alien, Synth, Nutural };
+    public TileType TileState; 
+    public bool IsNutural = false;
+    
+    // Use this for initialization
+	void Start () {
+        if (TileState == TileType.Alien)
+        {
+            this.renderer.material.mainTexture = AlienTexture;
+        }
+        else if (TileState == TileType.Synth)
+        {
+            this.renderer.material.mainTexture = SynthTexture;
+        }
+        else
+        {
+            this.renderer.material.mainTexture = NuturalTexture;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(TileState == TileType.Alien)
+        {
+            this.renderer.material.mainTexture = AlienTexture;
+        }
+        else if( TileState == TileType.Synth)
+        {
+            this.renderer.material.mainTexture = SynthTexture;
+        }
+        else
+        {
+            this.renderer.material.mainTexture = NuturalTexture;
+        }
+
+        if(IsNutural)
+        {
+            this.renderer.material.mainTexture = null;
+        }
+	}
+}
---
+++ ArchonClone/Assets/Scripts/OnTurnActions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* this script will contain all methods and variables used when moving 
+ * pieces and attacking during a turn, it will call A* to start a piece 
+ * moving and also test the distance to the target tile between the 
+ * current tile to check if it is a possible legal move. 
+ */
+
+public class OnTurnActions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
