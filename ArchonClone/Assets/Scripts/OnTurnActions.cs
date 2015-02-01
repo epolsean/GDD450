bb4e5939b30994a1f95fd11ed1b01769c2c5976a
blochowcizr <blochowiczr@yahoo.com>
2015-02-01 13:03:17 -0600

    fixedChanges
    i've deleted my copy of OnTurnActions as well as put in changes from
    curtis's camera script

--- ArchonClone/Assets/Scripts/OnTurnActions.cs
+++ ArchonClone/Assets/Scripts/OnTurnActions.cs
@@ -9,10 +9,11 @@ using Pathfinding;
  * tile to check if it is a possible legal move. 
  */
 
-public class OnTurnActions : MonoBehaviour {
+public class OnTurnActions : MonoBehaviour
+{
 
-    public Vector3 targetPosition; 
-    public CharacterController pieceCharacterController; 
+    public Vector3 targetPosition;
+    public CharacterController pieceCharacterController;
     public int MaxPathNodes;
     public float MaxMove;
     public GameObject SoundController;
@@ -21,31 +22,36 @@ public class OnTurnActions : MonoBehaviour {
     public GameObject OnHoverPiece;
     public GameObject CurrentTile;
     public GameObject SelectedPiece;
-    public bool hasSelectedPiece = false; 
+    public bool hasSelectedPiece = false;
     //Pathfinder variables
     public Seeker seeker;
     public Path path;
     public float nextWaypointDistance = 2f;
     public int currentWaypoint = 0;
-    public bool isGenPath = false; 
+    public bool isGenPath = false;
+    public bool drawnPath = false;
+    public GameObject[] allTiles;
 
-     
-    
+
+
     // Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    if(Input.GetMouseButtonDown(0))
+    void Start()
+    {
+        allTiles = GameObject.FindGameObjectsWithTag("Tile");
+        SoundController = GameObject.Find("UISoundController");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
         {
-            if(OnHoverTile != null)
+            if (OnHoverTile != null)
             {
                 if (hasSelectedPiece == false)
                 {
                     //check if piece is current players
-                    
+
                     if (OnHoverTile != null && OnHoverTile.GetComponent<OnTileActions>().PieceOnTile != null)
                     {
                         SelectPiece(OnHoverPiece);
@@ -59,20 +65,20 @@ public class OnTurnActions : MonoBehaviour {
                     }
                     else
                     {
-                        if (SelectedPiece.GetComponent<pieceMovementScript>().path.vectorPath.Count <= SelectedPiece.GetComponent<pieceMovementScript>().MaxPathNodes-1)
+                        if (SelectedPiece.GetComponent<pieceMovementScript>().path.vectorPath.Count <= SelectedPiece.GetComponent<pieceMovementScript>().MaxPathNodes)
                         {
                             SetTarget(OnHoverTile);
                         }
                         else
                         {
                             SoundController.GetComponent<UISoundsScript>().playError();
-                            ResetController(); 
+                            ResetController();
                         }
                     }
                 }
             }
         }
-	}
+    }
 
     /* called when you assign a selected piece to move
      * it assigns the selectedPiece, MaxMove, CurrentTile, 
@@ -84,18 +90,22 @@ public class OnTurnActions : MonoBehaviour {
         hasSelectedPiece = true;
         print("bool set to true");
         SelectedPiece = Piece;
-        CurrentTile = OnHoverTile; 
+        CurrentTile = OnHoverTile;
         //MaxMove = SelectedPiece.GetComponent<PiecePropScript>()
         //MaxPathNodes = SelectedPiece.GetComponent<PiecePropScript>()
-        OnHoverTile.GetComponent<OnTileActions>().isSelected = true; 
-        OnHoverTile.renderer.material.color = Color.yellow; 
+        OnHoverTile.GetComponent<OnTileActions>().isSelected = true;
+        OnHoverTile.renderer.material.color = Color.yellow;
+        if (SoundController != false)
+        {
+            SoundController.GetComponent<UISoundsScript>().playSelectPiece();
+        }
 
-        
+
     }
     /*when called the TurnController is reset to factory 
      * fresh settings this will be called after you move 
      * your piece or when you deselect your piece
-     */ 
+     */
     public void ResetController()
     {
         print("ResetController Called");
@@ -104,33 +114,49 @@ public class OnTurnActions : MonoBehaviour {
         CurrentTile.GetComponent<OnTileActions>().isSelected = false;
         MoveToTile.GetComponent<OnTileActions>().PieceOnTile = SelectedPiece;
         MoveToTile.renderer.material.color = Color.white;
-        MoveToTile.GetComponent<OnTileActions>().isSelected = false; 
+        MoveToTile.GetComponent<OnTileActions>().isSelected = false;
         SelectedPiece = null;
         CurrentTile = null;
-        MoveToTile = null; 
+        MoveToTile = null;
         MaxMove = 0;
         MaxPathNodes = 0;
         hasSelectedPiece = false;
+        resetAllTiles();
         Camera.main.GetComponent<CameraZoomController>().ResetTransform();
+
     }
-    
-    void SetTarget(GameObject targetTile)
+
+    void SetTarget(GameObject targetTile)//called when you select a tile to move to
     {
         SelectedPiece.GetComponent<pieceMovementScript>().isMoving = true;
-        SelectedPiece.GetComponent<pieceMovementScript>().startMove = true; 
-        targetTile.GetComponent<OnTileActions>().isSelected = true; 
+        SelectedPiece.GetComponent<pieceMovementScript>().startMove = true;
+        targetTile.GetComponent<OnTileActions>().isSelected = true;
         MoveToTile = targetTile;
         OnHoverTile.renderer.material.color = Color.green;
         hasSelectedPiece = false;
+        SelectedPiece.transform.position = new Vector3(SelectedPiece.transform.position.x, 0.1f, SelectedPiece.transform.position.z);//this is a quick fix for a weird bug where pice was being clocked from movng
+        if (SoundController != false)
+        {
+            SoundController.GetComponent<UISoundsScript>().playMovePiece();
+        }
         Camera.main.GetComponent<CameraZoomController>().FollowTarget(SelectedPiece);
     }
 
-    public void GenPath()
+    public void GenPath()//called to generated a path from the selectedPiece in the TurnController and the OnHoverTile(the tile you are hovering over)
     {
-        print("Generated Path"); 
-        SelectedPiece.GetComponent<pieceMovementScript>().targetPosition = OnHoverTile.transform.position; 
+        print("Generated Path");
+        SelectedPiece.GetComponent<pieceMovementScript>().targetPosition = OnHoverTile.transform.position;
         //SelectedPiece.GetComponent<pieceMovementScript>().GenNewPath();
-        SelectedPiece.GetComponent<pieceMovementScript>().GetNewPath(); 
-        isGenPath = true; 
+        SelectedPiece.GetComponent<pieceMovementScript>().GetNewPath();
+        isGenPath = true;
     }
+
+    public void resetAllTiles()//primarily used to reset the highlighted tiles in a path, it sets all tiles on the board to a base color of white
+    {
+        for (int i = 0; i < allTiles.Length; i++)
+        {
+            allTiles[i].renderer.material.color = Color.white;
+        }
+    }
+
 }
