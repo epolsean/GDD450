263a94767a3b8a390e1337bdfe09206934a91c40
polseane9067 <polseane9067@my.uwstout.edu>
2015-02-09 13:46:40 -0600

    Volume Controls Added

---
+++ ArchonClone/Assets/Scripts/VolumeController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class VolumeController : MonoBehaviour {
+
+    public GameObject VolumeSlider;
+	
+	// Update is called once per frame
+	void Update () {
+        AudioListener.volume = VolumeSlider.GetComponent<Slider>().value;
+	}
+}
